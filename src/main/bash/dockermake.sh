3711fe6b4cf4b6b52e5ef7f4d64af1e599274eb1
Jonathan Merritt <j.s.merritt@gmail.com>
2017-03-01 11:37:57 +1100

    TeamCity WIP

--- src/main/bash/dockermake.sh
+++ src/main/bash/dockermake.sh
@@ -19,8 +19,7 @@
 # Example Usage:
 # ./ci/dockermake.sh build myimage1 myimage2 --build-arg=someval
 
-
-set -u
+set -x
 
 # Library import helper
 function import() {
@@ -68,7 +67,7 @@ FLAGS=("$@")
 # Find all the docker images, use the directory structure for their path.
 while read i ; do
     IMAGES+=( "$(dirname $i)" )
-done < <(find * -name Dockerfile -type f -print | sort)
+done < <(find -H -L * -name Dockerfile -type f -print | sort)
 
 # Find the images we were requested to build, or build all if none specified.
 # Note: this preserves build order to command line order.
@@ -106,9 +105,9 @@ do_build() {
 
     for i in ${TOBUILD[@]}; do
         docker build ${BUILD_ARGS[@]} \
-                     --build-arg=http_proxy=$http_proxy \
-                     --build-arg=https_proxy=$http_proxy \
-                     --build-arg=ftp_proxy=$http_proxy \
+                     --build-arg=http_proxy="$http_proxy" \
+                     --build-arg=https_proxy="$http_proxy" \
+                     --build-arg=ftp_proxy="$http_proxy" \
                      ${FLAGS[@]} \
                      -t ${PREFIX}${i}:${TAG} ${i}
          exit_code=$?
